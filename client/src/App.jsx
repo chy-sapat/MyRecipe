5e1ba6fe52bf87342a1e7c7bfec8611fe6b99538
neha maharjan <maharjanneha44@gmail.com>
2024-02-07 18:06:47 +0545

    install some library

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,17 @@ import Auth from "./pages/auth";
 function App() {
   return (
     <>
-      <Auth />
+      <div className="container">
+        <section className="image"></section>
+        <section className="header">Sign in</section>
+        <form action="" method="post">
+          <label htmlFor="username">Username</label>
+          <input type="text" id="username" />
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" />
+          <button type="submit"></button>
+        </form>
+      </div>
     </>
   );
 }
